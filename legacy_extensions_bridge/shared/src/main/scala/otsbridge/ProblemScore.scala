9be6e20c07ac72a5f6b6d4731f7a19afc4dc4df2
azu0nyan <nikgavgav@gmail.com>
2020-08-11 21:52:57 +0300

    Some multiple runs work

--- legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProblemScore.scala
+++ legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProblemScore.scala
@@ -7,13 +7,22 @@ object ProblemScore{
     case (IntScore(p1), IntScore(p2)) => if (p1> p2) s1 else s2
     case (DoubleScore(p1), DoubleScore(p2)) => if (p1> p2) s1 else s2
     case (XOutOfYScore(p1, _), XOutOfYScore(p2, _)) => if (p1> p2) s1 else s2
+    case (m1@MultipleRunsResultScore(_), m2@MultipleRunsResultScore(_)) => if (m1.successesTotal> m2.successesTotal) s1 else s2
     case _ => s1
   }
 }
+
+
 
 sealed trait ProblemScore
 case class BinaryScore(passed: Boolean) extends ProblemScore
 case class IntScore(score: Int) extends ProblemScore
 case class DoubleScore(score: Int) extends ProblemScore
 case class XOutOfYScore(score: Int, maxScore: Int) extends ProblemScore
+case class MultipleRunsResultScore(runResults:Seq[ProgramRunResult]) extends ProblemScore {
+  def successesTotal :Int = runResults.count(_.success)
+  def tasksTotal:Int  = runResults.count(_.success)
+  def toXOutOfY:XOutOfYScore = XOutOfYScore(successesTotal, tasksTotal)
+}
 
+case class ProgramRunResult(success:Boolean, timeSeconds: Double, message:Option[String])
