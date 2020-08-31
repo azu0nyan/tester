e2d9650bf59fcb50d9c4ade015aa4747bf08fee0
azu0nyan <nikgavgav@gmail.com>
2020-08-31 23:21:03 +0300

    course contents display

--- legacy_extensions_bridge/shared/src/main/scala/otsbridge/CoursePiece.scala
+++ legacy_extensions_bridge/shared/src/main/scala/otsbridge/CoursePiece.scala
@@ -1,25 +1,41 @@
 package otsbridge
 
-import otsbridge.DisplayMe.{ Inline, OwnPage}
+import otsbridge.DisplayMe.{Inline, OwnPage}
 
 object CoursePiece {
   val emptyCourse: CourseRoot = CourseRoot("", "", Seq())
 
 
-
+  val pathDelimiter = "#"
+  type PiecePath = Seq[String]
+  def pathToString(path: PiecePath): String = path.mkString(pathDelimiter)
+  def stringToPath(str: String): PiecePath = str.split(pathDelimiter).toSeq
+  type PieceWithPath = (CoursePiece, PiecePath)
 
   sealed trait CoursePiece {
     def alias: String
     def displayMe: DisplayMe
+
+    val displayInContentsHtml: Option[String] = None
+
+    lazy val linearize: Seq[PieceWithPath] = Seq((this, Seq(alias)))
+
+    lazy val pieceByPath:Map[String, CoursePiece] = linearize.map(x => (pathToString(x._2), x._1)).toMap
+
   }
 
   sealed trait Container extends CoursePiece {
     def childs: Seq[CoursePiece]
+    override lazy val linearize: Seq[PieceWithPath] = (this, Seq(alias)) +:
+      childs.flatMap(_.linearize).map { case (piece, path) => (piece, alias +: path)
+      }
   }
 
-  case class CourseRoot(title: String, annotation: String, childs: Seq[CoursePiece] ) extends Container {
+  case class CourseRoot(title: String, annotation: String, childs: Seq[CoursePiece]) extends Container {
     override def alias: String = "main"
     override def displayMe: DisplayMe = OwnPage
+
+    override val displayInContentsHtml: Option[String] = Some(s"<h2>$title</h2>")
   }
 
 
@@ -27,10 +43,11 @@ object CoursePiece {
                     alias: String,
                     title: String,
                     textHtml: String = "",
-                    childs: Seq[CoursePiece]= Seq(),
-                    displayMe:DisplayMe = OwnPage
+                    childs: Seq[CoursePiece] = Seq(),
+                    displayMe: DisplayMe = OwnPage
                   ) extends Container {
-//    override def displayMe: DisplayMe = OwnPage
+    //    override def displayMe: DisplayMe = OwnPage
+    override val displayInContentsHtml: Option[String] = Some(s"<h3>$title</h3>")
   }
 
   case class SubTheme(
@@ -38,18 +55,21 @@ object CoursePiece {
                        title: String,
                        textHtml: String = "",
                        childs: Seq[CoursePiece] = Seq(),
-                       displayMe:DisplayMe = OwnPage
+                       displayMe: DisplayMe = OwnPage
                      ) extends Container {
-//    override def displayMe: DisplayMe = OwnPage
+    override val displayInContentsHtml: Option[String] = Some(s"<h4>$title</h4>")
+    //    override def displayMe: DisplayMe = OwnPage
   }
 
 
-  case class HtmlToDisplay(alias: String, displayMe: DisplayMe, htmlRaw:String) extends CoursePiece
+  case class HtmlToDisplay(alias: String, displayMe: DisplayMe, htmlRaw: String) extends CoursePiece
 
-  case class TextWithHeading(alias:String,heading:String, bodyHtml:String,  displayMe: DisplayMe = Inline) extends CoursePiece
-  case class Paragraph(alias:String, bodyHtml:String,  displayMe: DisplayMe = Inline) extends CoursePiece
+  case class TextWithHeading(alias: String, heading: String, bodyHtml: String, displayMe: DisplayMe = Inline) extends CoursePiece
+
+  case class Paragraph(alias: String, bodyHtml: String, displayMe: DisplayMe = Inline) extends CoursePiece
 
   case class Problem(problemAlias: String, displayMe: DisplayMe) extends CoursePiece {
+//    override val displayInContentsHtml: Option[String] = Some(s"<h2>$contentsTitle</h2>")
     override def alias: String = problemAlias
   }
 
