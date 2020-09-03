0cff7eb221946c0350c3d17ce8b77810b8c10907
azu0nyan <nikgavgav@gmail.com>
2020-09-03 15:08:14 +0300

    some sh scripts

--- legacy_ots/remoteScripts/deployUniverrsalToVPS.sh
+++ legacy_ots/remoteScripts/deployUniverrsalToVPS.sh
@@ -11,7 +11,6 @@ DIR="tester"
 sshpass -p ${PASSWORD} ssh ${USER}@${HOST} "killall -9 java"
 sshpass -p ${PASSWORD} ssh ${USER}@${HOST} "rm ~/${DIR} -r;cd ~; mkdir ${DIR}"
 cd ../jvm/target/universal || exit
-ls | grep .zip
 sshpass -p ${PASSWORD} scp ${FILE} ${USER}@${HOST}:~/${DIR}/${FILE}
 sshpass -p ${PASSWORD} ssh ${USER}@${HOST} "cd ~/${DIR};unzip ${FILE};cd online-test-suite-0.2-SNAPSHOT/bin;./online-test-suite"
 
