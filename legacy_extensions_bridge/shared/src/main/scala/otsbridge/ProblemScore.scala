8e302bc25e78a907f583a3f3da1b1ac35e17cb70
azu0nyan <nikgavgav@gmail.com>
2020-09-28 18:16:05 +0300

    grades backend

--- legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProblemScore.scala
+++ legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProblemScore.scala
@@ -3,26 +3,37 @@ package otsbridge
 import otsbridge.ProgramRunResult._
 
 object ProblemScore {
-  sealed trait ProblemScore{
-    def toPrettyString:String
+  sealed trait ProblemScore {
+    def toPrettyString: String
 
-    def toInt:Int
+    def toInt: Int
+
+    def isMax: Boolean
+
+    def percentage: Double
   }
+
   case class BinaryScore(passed: Boolean) extends ProblemScore {
-    override def toInt: Int = if(passed) 1 else 0
+    override def toInt: Int = if (passed) 1 else 0
     override def toPrettyString: String = passed.toString
+    override def isMax: Boolean = passed
+
+    override def percentage: Double = toInt.toDouble
+
   }
-  case class IntScore(score: Int) extends ProblemScore {
-    override def toInt: Int = score
-    override def toPrettyString: String = score.toString
-  }
+
   case class DoubleScore(score: Double, maxScore: Double) extends ProblemScore {
-    override def toInt: Int =  math.ceil(score).toInt
+    override def toInt: Int = math.ceil(score).toInt
     override def toPrettyString: String = s"${score.toString} / ${maxScore.toString}"
+    override def isMax: Boolean = score == maxScore
+
+    override def percentage: Double = score / maxScore
   }
   case class XOutOfYScore(score: Int, maxScore: Int) extends ProblemScore {
     override def toInt: Int = score
     override def toPrettyString: String = s"${score} / ${maxScore.toString}"
+    override def isMax: Boolean = score == maxScore
+    override def percentage: Double = score.toDouble / maxScore.toDouble
   }
   case class MultipleRunsResultScore(runResults: Seq[ProgramRunResult]) extends ProblemScore {
 
@@ -31,6 +42,10 @@ object ProblemScore {
     //  def toXOutOfY:XOutOfYScore = XOutOfYScore(successesTotal, tasksTotal)
     override def toInt: Int = successesTotal
     override def toPrettyString: String = s"${successesTotal} / ${tasksTotal.toString}"
+
+    override def isMax: Boolean = successesTotal == tasksTotal
+
+    override def percentage: Double = successesTotal.toDouble / tasksTotal.toDouble
   }
 }
 
