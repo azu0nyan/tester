dc68cb9e9fe7e51a19ec7bf4cfeb6e7f9f6143d8
azu0nyan <nikgavgav@gmail.com>
2020-11-11 01:23:55 +0300

    Remote languages support

--- legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProgrammingLanguage.scala
+++ legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProgrammingLanguage.scala
@@ -2,10 +2,11 @@ package otsbridge
 
 object ProgrammingLanguage {
 
-  val ALL_LANGUAGES = Seq(Java, Scala, Cpp)
+  val ALL_LANGUAGES = Seq(Java, Scala, Cpp, Haskell)
 
   sealed trait ProgrammingLanguage
   case object Java extends ProgrammingLanguage
+  case object Haskell extends ProgrammingLanguage
   case object Scala extends ProgrammingLanguage
   case object Cpp extends ProgrammingLanguage
 }
