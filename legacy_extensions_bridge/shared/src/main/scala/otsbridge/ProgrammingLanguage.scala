f5c18d97624820a983231fa97fb48d23a57e3fd3
azu0nyan <nikgavgav@gmail.com>
2022-09-25 15:50:05 +0300

    kojo enviroment

--- legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProgrammingLanguage.scala
+++ legacy_extensions_bridge/shared/src/main/scala/otsbridge/ProgrammingLanguage.scala
@@ -2,12 +2,13 @@ package otsbridge
 
 object ProgrammingLanguage {
 
-  val ALL_LANGUAGES = Seq(Java, Scala, Cpp, Haskell)
+  val ALL_LANGUAGES = Seq(Java, Scala, Cpp, Haskell, Kojo)
 
   sealed trait ProgrammingLanguage
   case object Java extends ProgrammingLanguage
   case object Haskell extends ProgrammingLanguage
   case object Scala extends ProgrammingLanguage
+  case object Kojo extends ProgrammingLanguage
   case object Cpp extends ProgrammingLanguage
 }
 
