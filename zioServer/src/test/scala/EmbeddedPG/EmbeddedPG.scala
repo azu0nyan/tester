5064f6da92984be83cdbe4f4588ce4526d5d961a
azu0nyan <nikgavgav@gmail.com>
2023-08-14 14:47:46 +0300

    Embedded postgres working

--- zioServer/src/test/scala/EmbeddedPG/EmbeddedPG.scala
+++ zioServer/src/test/scala/EmbeddedPG/EmbeddedPG.scala
@@ -11,16 +11,36 @@ import scala.io.Source
 import io.github.gaelrenoux.tranzactio.doobie.*
 
 object EmbeddedPG {
-  val schemaSql: String = Source.fromResource("schema.sql").getLines().mkString("\n")
+  val schemaSqlLines = Source.fromResource("schema.sql").getLines()
+  val sqlStatements: Seq[String] =  schemaSqlLines
+    .filter(str => !str.isBlank)
+    .filter(str => !str.startsWith("--"))
+    .mkString("\n")
+    .split(";")
+    .map(_ + ";")
+
+  def initDb(db: DataSource) : Unit = {
+    val conn = db.getConnection
+    println(s"Creating test schema")
+    val stmt = conn.createStatement()
+    for(sql <- sqlStatements) {
+      println(s"Executing ${sql}")
+      val initResult = stmt.execute(sql)
+      println(s"Exec result $initResult")
+    }
+    stmt.close()
+    println(s"Db init finished")
+    conn.close()
+
+  }
 
   //LC_ALL=en_US.UTF-8;LC_CTYPE=en_US.UTF-8
   val dataSource: Task[DataSource] =
     ZIO.succeed {
       val epg = EmbeddedPostgres.builder()
         .start()
-      val conn = epg.getPostgresDatabase.getConnection
-      conn.prepareStatement(schemaSql).execute()
-      conn.close()
+      initDb(epg.getPostgresDatabase)
+
       epg.getPostgresDatabase
     }
 
@@ -28,16 +48,16 @@ object EmbeddedPG {
 
   val databaseLayer: ZLayer[Any, Throwable, Database] = dataSourceLayer >>> Database.fromDatasource
 
-  //  val connectionLayer: ZLayer[Database, Throwable, Connection] =
-  //    ZIO.serviceWith[Database]{ db => db.transactionOrDie()
-  //
-  //    }
   val connection: Task[Connection] = for {
     ds <- dataSource
-    conn <- Database.connectionFromJdbc(ds.getConnection)
+    conn <- Database.connectionFromJdbc{
+      val res = ds.getConnection
+      res.setSchema("tester")
+      res
+    }
   } yield conn
 
-  val connectionLayer = ZLayer.fromZIO(connection)
+  val connectionLayer: ZLayer[Any, Throwable, Connection] = ZLayer.fromZIO(connection)
 
   //    val transactor = Transactor.fromDriverManager(
   //      "org.postgresql.Driver",
